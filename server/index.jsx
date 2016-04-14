a1989935a7e3e3194b317d13d4004b9b923ce205
skasch <romain.mondoncancel@gmail.com>
2016-04-14 20:56:23 +0200

    Production bug fixing

--- server/index.jsx
+++ server/index.jsx
@@ -33,6 +33,7 @@ function renderTemplate(initialState) {
 	<!DOCTYPE html>
   <html>
     <head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
     	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 			<style>
